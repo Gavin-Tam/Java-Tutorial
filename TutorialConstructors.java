725fd99aa74e05adcefdb2c1198776ed5dc86992
Gavin Nguyen <147111953+Gavin-Tam@users.noreply.github.com>
2023-10-07 12:09:35 -0400

    Update TutorialConstructors.java

--- TutorialConstructors.java
+++ TutorialConstructors.java
@@ -1,11 +1,16 @@
-
 public class TutorialConstructors {
 
 	public static void main(String[] args) {
-	Shirt s = new Shirt("White", 'M');
+	Shirt s1 = new Shirt("White", 'M');
+	Shirt s2 = new Shirt("White", 'M',"cotten");
 	
-System.out.println(s.color);
-System.out.println(s.size);
+System.out.println(s1.color);
+System.out.println(s1.size);
+System.out.println("--------------");
+System.out.println(s2.color);
+System.out.println(s2.size);
+System.out.println(s2.material);
 	}
 
 }
+
